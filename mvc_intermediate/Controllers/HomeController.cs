6738f131ee9057899e8f845a25c44f17114fd9fc
ilaydakyer <ilaydkyr@gmail.com>
2025-07-21 11:28:30 +0300

    razer kullanımı ve amacı öğrenildi

---
+++ mvc_intermediate/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_intermediate.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //gerçek senaryoda homecontroller;
+            //veritabanına bağlanır ve bilgileri getirir.
+            //getirilen bilgiler bir model içerisine yani 
+            //bir sınıf içerisine aktarılır
+            //ve bu model view'a gönderilir
+            return View();
+        }
+    }
+}
