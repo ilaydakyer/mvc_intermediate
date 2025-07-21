4a74d225f7976d87bf5974f6a702b635f752b468
ilaydakyer <ilaydkyr@gmail.com>
2025-07-21 13:43:10 +0300

    model yapısı güncel, butonlar ve html yapısı güncellendi

--- mvc_intermediate/Controllers/HomeController.cs
+++ mvc_intermediate/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using mvc_intermediate.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Windows.Documents;
 
 namespace mvc_intermediate.Controllers
 {
@@ -17,10 +19,17 @@ namespace mvc_intermediate.Controllers
             //bir sınıf içerisine aktarılır
             //ve bu model view'a gönderilir
 
-            string[] kurslar = { "mvc kursu", "web form kursu", "c# kursu" };
-
-
-            return View(kurslar);
+            List < Urun > urunler = new List<Urun>() 
+            {
+                new Urun(){UrunID=1, UrunAdi="Samsung S6", Aciklama="idare eder", Satista_mi=true},
+                new Urun(){UrunID=2, UrunAdi="Samsung S7", Aciklama="idare eder", Satista_mi=true},
+                new Urun(){UrunID=3, UrunAdi="Samsung S8", Aciklama="idare eder", Satista_mi=false},
+                new Urun(){UrunID=4, UrunAdi="Iphone 6", Aciklama="idare eder", Satista_mi=true},
+                new Urun(){UrunID=5, UrunAdi="Iphone 7", Aciklama="idare eder", Satista_mi=true},
+                new Urun(){UrunID=6, UrunAdi="Iphone 7", Aciklama="idare eder", Satista_mi=false},
+                new Urun(){UrunID=7, UrunAdi="Iphone 7", Aciklama="idare eder", Satista_mi=true},
+            };
+            return View(urunler);
         }
     }
 }
---
+++ mvc_intermediate/Models/Urun.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc_intermediate.Models
+{
+    public class Urun
+    {
+        public int UrunID { get; set; }
+        public string UrunAdi { get; set; }
+        public string Aciklama { get; set; }
+        public bool Satista_mi {  get; set; }
+    }
+}
